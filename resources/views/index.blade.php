7a15c3d0e3e7b9e56db8e97c39bd963a8bec414e
CarloVeronese <carlo.veronese97@gmail.com>
2023-12-11 16:55:05 +0100

    add - page-header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <section>
         <div class="container">
-            <h1>Pagina Home</h1>
         </div>
     </section>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,31 @@
-<header class="page-header">
+@php
+ $menu = ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP'];   
+@endphp
+
+<header class="page-header" style="background-image: url( {{ Vite::asset('resources/img/jumbotron.jpg') }})">
     <div class="page-top">
         <div class="container">
-            <ul class="row justify-content-end mb-0">
+            <ul class="row justify-content-end">
                 <li class="col-auto">DC POWER VISA &reg;</li>
                 <li class="col-auto">ADDITIONAL DC SITES</li>
             </ul>
+        </div>
+    </div>
+    <div class="nav-bar bg-light">
+        <div class="container">
+            <div class="row flex-nowrap align-items-center">
+                <div class="col-auto">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="" class="logo">
+                </div>
+                <ul class="col d-flex flex-grow-1 justify-content-between">
+                    @foreach ($menu as $item)
+                        <li><a href="" class="text-decoration-none text-dark fw-bold">{{ $item }}</a></li>                        
+                    @endforeach
+                </ul>
+                <div class="col-auto search">
+                    <input type="text" placeholder="search" class="border border-0 search-bar">
+                </div>
+            </div>
         </div>
     </div>
 </header>
