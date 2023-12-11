ffb63d33ec4367e082a2da7951b34c6f2c13fa18
CarloVeronese <carlo.veronese97@gmail.com>
2023-12-11 20:21:33 +0100

    add - load more button

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,13 +4,16 @@
     <section class="comics">
         <div class="container">
             <div class="current-series"> CURRENT SERIES</div>
-            <div class="row comics-list">
-                @foreach ($comics as $item)
-                    <div class="col-2 comic-container">
-                        <img src="{{$item['thumb']}}" alt="" class="thumb">
-                        <h3 class="series">{{strtoupper($item['series'])}}</h3>
-                    </div>
-                @endforeach
+            <div class="main-content d-flex flex-column">
+                <div class="row comics-list">
+                    @foreach ($comics as $item)
+                        <div class="col-2 comic-container">
+                            <img src="{{$item['thumb']}}" alt="" class="thumb">
+                            <h3 class="series">{{strtoupper($item['series'])}}</h3>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="load-more d-flex justify-content-center"><h4>LOAD MORE</h4></div>
             </div>
         </div>
     </section>
