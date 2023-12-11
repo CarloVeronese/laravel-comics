19f84b2416724530cf7ddbac6ee8d34d59ed7340
CarloVeronese <carlo.veronese97@gmail.com>
2023-12-11 16:13:12 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        <div class="container">
+            <h1>Pagina Home</h1>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header class="page-header">
+    <div class="page-top">
+        <div class="container">
+            <ul class="row justify-content-end mb-0">
+                <li class="col-auto">DC POWER VISA &reg;</li>
+                <li class="col-auto">ADDITIONAL DC SITES</li>
+            </ul>
+        </div>
+    </div>
+</header>
